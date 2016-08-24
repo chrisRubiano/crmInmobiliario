827d6de5dd29ebbfb4c780fbc1037a59b415e110
MClark <miguelclark@hotmail.com>
2016-08-24 10:56:36 -0700

    CRUD Roles de usuario

---
+++ crmInmobiliario/Controllers/AspNetRolesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using crmInmobiliario.Models;
+
+namespace crmInmobiliario.Controllers
+{
+    [Authorize]
+    public class AspNetRolesController : Controller
+    {
+        private CRMINMOBILIARIOEntities10 db = new CRMINMOBILIARIOEntities10();
+
+        // GET: AspNetRoles
+        public ActionResult Index()
+        {
+
+            var roles = from r in db.AspNetRoles
+                            select r;
+            return View(roles.OrderBy(r => r.Name).ToList());
+            
+        }
+
+        // GET: AspNetRoles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetRoles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] AspNetRoles aspNetRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetRoles.Add(aspNetRoles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRoles);
+        }
+
+        // POST: AspNetRoles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] AspNetRoles aspNetRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetRoles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRoles);
+        }
+
+        // POST: AspNetRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            db.AspNetRoles.Remove(aspNetRoles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
