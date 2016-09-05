ae2cf0d753fe1999a1e1dd09edc786079bd3f462
MClark <miguelclark@hotmail.com>
2016-09-04 19:58:57 -0700

    Data Annotations de Propiedades, Edificios, Domicilios, Desarrollos, Medios de Contacto

--- crmInmobiliario/Models/Domicilios.cs
+++ crmInmobiliario/Models/Domicilios.cs
@@ -11,7 +11,10 @@ namespace crmInmobiliario.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
+
+    [MetadataType(typeof(DomiciliosMeta))]
     public partial class Domicilios
     {
         public int IdDomicilio { get; set; }
---
+++ crmInmobiliario/Models/DomiciliosMeta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace crmInmobiliario.Models
+{
+    public class DomiciliosMeta
+    {
+
+         [Display(Name = "Num Exterior")]        
+        public string NumExterior { get; set; }
+
+         [Display(Name = "Num Interior")]        
+        public string NumInterior { get; set; }
+
+         [Display(Name = "Entre/Esquina")]        
+        public string EntreEsquina { get; set; }
+
+         [Display(Name = "Y Calle")]        
+        public string YCalle { get; set; }
+
+    }
+}
