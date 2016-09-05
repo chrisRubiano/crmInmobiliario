39a1b696ed80321aa7da162f61d7122fa5e1f252
MClark <miguelclark@hotmail.com>
2016-09-04 22:24:59 -0700

    Data Annotations de Amortizaciones y Cotizaciones, Avance de estilo tipo Wizard para Ctoizaciones

--- crmInmobiliario/Models/AmortizacionMeta.cs
+++ crmInmobiliario/Models/AmortizacionMeta.cs
@@ -9,9 +9,18 @@ namespace crmInmobiliario.Models
     public class AmortizacionMeta
     {
         [Display(Name = "Fecha Programada")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaProgramado { get; set; }
+
+        [DataType(DataType.Currency)]
+        public Nullable<decimal> Importe { get; set; }
+
         [Display(Name = "Está Pagado")]
         public Nullable<bool> EstaPagado { get; set; }
+
+        [Display(Name = "Cotización")]
+        public Nullable<int> Cotizacion { get; set; }
 
     }
 }
