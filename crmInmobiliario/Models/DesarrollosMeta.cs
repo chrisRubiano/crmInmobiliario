f0fd8ab49deabe110a92ba31ae0d300d7b16da5b
MClark <miguelclark@hotmail.com>
2016-09-21 08:29:47 -0700

    Se actualizaorn Data annotations de Desarrollos

--- crmInmobiliario/Models/DesarrollosMeta.cs
+++ crmInmobiliario/Models/DesarrollosMeta.cs
@@ -12,16 +12,22 @@ namespace crmInmobiliario.Models
         [StringLength(150)]
         public string Desarrollo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe Caoturar la Clave del Desarrollo")]
         [StringLength(2)]
         public string Clave { get; set; }
     
-        [Required]
+        
+        [Required(ErrorMessage = "Debe Capturar el % de Descuento Máximo")]
         [Display(Name = "% Descuento")]        
         public Nullable<decimal> Descuento { get; set; }
         
         [Display(Name = "Cajones de Estacionamiento")]
         public Nullable<int> CajonesEstacionamiento { get; set; }
+
+        [Display(Name = "Fecha de Entrega")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> FechaEntrega { get; set; }
         
     }
 }
