fc20e13dc4d628a4afd544636c9b940fb46c0958
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-10-05 08:35:38 -0700

    modelo actualizado :eyes:

--- crmInmobiliario/Models/modeloCRM.Context.cs
+++ crmInmobiliario/Models/modeloCRM.Context.cs
@@ -74,5 +74,6 @@ namespace crmInmobiliario.Models
         public virtual DbSet<ProspectosIncidencias> ProspectosIncidencias { get; set; }
         public virtual DbSet<Bitacora> Bitacora { get; set; }
         public virtual DbSet<FormasPago> FormasPago { get; set; }
+        public virtual DbSet<EstadoCivil> EstadoCivil { get; set; }
     }
 }
