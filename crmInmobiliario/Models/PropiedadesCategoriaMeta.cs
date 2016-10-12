bb1369ae3e68921f3edf15bf5bee58b5644f7424
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-10-12 09:00:15 -0700

    validaciones y correcciones en metadatos

---
+++ crmInmobiliario/Models/PropiedadesCategoriaMeta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crmInmobiliario.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class PropiedadesCategoriaMeta
+    {
+        public int IdCategoria { get; set; }
+        public string Categoria { get; set; }
+        [Required, StringLength(1)]
+        public string Clave { get; set; }
+        public Nullable<bool> Activo { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Propiedades> Propiedades { get; set; }
+    }
+}
