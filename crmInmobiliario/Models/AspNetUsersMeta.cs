a5edf765ca275ff4073744e339e56369213a7e7f
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-10-12 22:27:44 -0700

    rol de usuario en lista y menu de roles oculto :pineapple:

---
+++ crmInmobiliario/Models/AspNetUsersMeta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crmInmobiliario.Models
+{
+    public class AspNetUsersMeta
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+        [Display(Name = "Rol de usuario")]
+        public string UserRoles { get; set; }
+    }
+}
